455f2f94248738234df9173bf578cfdfa22b3da6
daniel.wladdimiro <daniel.wladdimiro@gmail.com>
2016-06-29 12:57:31 -0400

    Clean project

--- statisticsFinal/App 1/Normal/Con monitor/plot/plotAll.sh
+++
@@ -1,9 +0,0 @@
-#java -jar ReadFile.jar
-
-for file in ./*
-do
-	if [ ${file: -4} == ".tpl" ]
-	then	
-		gnuplot $file
-	fi
-done
--- statisticsFinal/App 3/Uniform/Con monitor/Fisical/plot/plotAll.sh
+++
@@ -1,7 +0,0 @@
-for file in ./*
-do
-	if [ ${file: -4} == ".tpl" ]
-	then	
-		gnuplot $file
-	fi
-done
